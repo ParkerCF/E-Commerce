733d8455924e6549196ffafd30043324a4b03da0
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-02-18 13:11:53 -0500

    fixed problem in tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,7 @@ class Tag extends Model {}
 Tag.init(
   {
     id: {
-      type: DataTypes.INTEGER,,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
